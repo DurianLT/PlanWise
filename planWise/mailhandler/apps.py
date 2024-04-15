a8c8d01f8ba0b0db3cdc015b0e995225c04d4c8a
Fallen-Dynasty <145264713+Fallen-Dynasty@users.noreply.github.com>
2024-04-15 19:45:08 +0800

    Delete planWise directory
    Upload wrong path.

--- planWise/mailhandler/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MailhandlerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mailhandler'
--- planWise/mailhandler/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('emails/', views.EmailListView.as_view(), name='email-list'),
-    path('emails/<int:pk>/', views.EmailDetailView.as_view(), name='email-detail'), 
-    path('calendar/', views.CalendarView.as_view(), name='calendar-view'),
-]
