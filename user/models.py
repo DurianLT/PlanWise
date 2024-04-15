a479df987862f74dcb709ae0d5f33d306d747275
DurianLT <161314850+DurianLT@users.noreply.github.com>
2024-04-15 20:02:33 +0800

    Delete user directory
    Update wrong path

--- user/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-    outlook_email = models.EmailField('Outlook 邮箱', unique=True)
-    secondary_password = models.CharField('二级密码', max_length=50)
