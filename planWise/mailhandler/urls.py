b9c70318bab63a841b716571b218f2954a862d10
DurianLT <liutianjunrong@outlook.com>
2024-04-19 15:20:51 +0800

    update urls

--- planWise/mailhandler/urls.py
+++ planWise/mailhandler/urls.py
@@ -1,6 +1,6 @@
 # 在你的 urls.py 文件中
 from django.urls import path
-from .views import CheckUserView, DisplayEmailsView, DisplayEmailView, GetEmailsView, UpdateUserView, EmailDetailView
+from .views import CheckUserView,UpdateUserView, EmailDetailView
 
 urlpatterns = [
     path('check-users/', CheckUserView.as_view(), name='check-users'),
