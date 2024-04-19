253c2fa8057c3bf35b0d72ccf4332b174375e496
DurianLT <liutianjunrong@outlook.com>
2024-04-19 15:20:27 +0800

    clear project

--- planWise/mailhandler/urls.py
+++ planWise/mailhandler/urls.py
@@ -4,9 +4,6 @@ from .views import CheckUserView, DisplayEmailsView, DisplayEmailView, GetEmails
 
 urlpatterns = [
     path('check-users/', CheckUserView.as_view(), name='check-users'),
-    path('display-emails/', DisplayEmailsView.as_view(), name='display-emails'),
-    path('get-emails/<int:user_id>/', GetEmailsView.as_view(), name='get-emails'),
-    path('display-email/', DisplayEmailView.as_view(), name='display-email'),
     path('update-email/', UpdateUserView.as_view(), name='update-email'),
     path('email/<int:pk>/', EmailDetailView.as_view(), name='email_detail'),
 ]
