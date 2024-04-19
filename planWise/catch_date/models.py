44226ceb944be170af5f8747ef30f8f8ec08325c
Fallen-Dynasty <281546476@qq.com>
2024-04-19 19:50:54 +0800

    Update catch_date

--- planWise/catch_date/models.py
+++ planWise/catch_date/models.py
@@ -7,6 +7,6 @@ class Event(models.Model):
     date = models.DateTimeField()
     user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
     address = models.CharField(max_length=255)
-    event = models.CharField(max_length=255)
+    event = models.TextField()
     comment = models.CharField(max_length=255)
     
