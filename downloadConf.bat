cde5e5052d614eaef54ff952698898b450631260
DurianLT <liutianjunrong@outlook.com>
2024-04-20 17:50:33 +0800

    update bat

--- downloadConf.bat
+++ downloadConf.bat
@@ -1,4 +1,7 @@
 @echo off
 cd /d %~dp0
 pip3 install -r requirements.txt
+cd /d %~dp0/planWise
+python manage.py makemigrations
+python manage.py migrate
 pause
--- start.bat
+++ start.bat
@@ -1,5 +1,4 @@
 @echo off
 cd /d %~dp0/planWise
-python manage.py makemigrations
-python manage.py migrate
+python manage.py runserver
 pause
